2188e20b91d89ce8e8e2b4a00c4c35d2d8ab016d
Shahid Arshad <shahid.arshad@devsinc.com>
2023-02-27 11:03:34 -0500

    Showing mission details with ScrollView and GeometryReader

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -22,8 +22,47 @@ struct ContentView: View {
     
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     var body: some View {
-        Text("\(astronauts.count)")
+        NavigationView{
+            ScrollView {
+                LazyVGrid(columns: columns){
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail View")
+                        } label : {
+                            VStack{
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                      .padding(5)
+                                    .frame(width: 100, height: 100)
+                                VStack{
+                                    Text(mission.displayNmae)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(mission.formattedLaunchdate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground))
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+        }
     }
 }
 
