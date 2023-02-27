b924ae85264f45d73718d5a303517d7da4a40453
Shahid Arshad <shahid.arshad@devsinc.com>
2023-02-27 05:24:49 -0500

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Dev on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
